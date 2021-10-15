fcc11a1da56c3a7272089e16949344c070f3552e
hurenbin.hrb <hurenbin.hrb@antgroup.com>
2021-10-15 17:00:48 +0800

    feat: init canvas debugger

---
+++ src/panel/panel.jsx
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+const Index = () => {
+
+  return (
+    <h1>这里是Canvas Debugger的devtools界面。</h1>
+  );
+};
+
+ReactDOM.render(<Index />, document.getElementById('root'));
